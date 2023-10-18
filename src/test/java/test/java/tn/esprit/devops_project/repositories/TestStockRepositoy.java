8c74f47a9b80afe07b11ca4f26a341326dd83a03
emna-hannachii <emna.hannachi@esprit.tn>
2023-10-18 21:23:37 +0100

    change the name of repo test

--- src/test/java/test/java/tn/esprit/devops_project/repositories/TestStockRepositoy.java
+++ src/test/java/test/java/tn/esprit/devops_project/repositories/TestStockRepositoy.java
@@ -2,28 +2,15 @@ package test.java.tn.esprit.devops_project.repositories;
 
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 import tn.esprit.devops_project.DevOps_ProjectSpringBootApplication;
-import tn.esprit.devops_project.entities.Stock;
-import tn.esprit.devops_project.repositories.StockRepository;
-import tn.esprit.devops_project.services.StockServiceImpl;
-
-import javax.transaction.Transactional;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @Slf4j
 @SpringBootTest(classes = DevOps_ProjectSpringBootApplication.class)
-public class StockRepositoyTest {
+public class TestStockRepositoy {
   /*  @Autowired private StockRepository stockRepository;
 
     @Autowired private StockServiceImpl stockService;
