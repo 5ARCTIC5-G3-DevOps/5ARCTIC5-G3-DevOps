7ab93c8b9848add24ead11d2b531c98f5cc81e87
sahar-letaief <sahar.letaief@esprit.tn>
2023-10-24 23:20:13 +0100

    sonarlint + jococo

--- src/main/java/tn/esprit/devops_project/services/Iservices/IInvoiceDetailService.java
+++ src/main/java/tn/esprit/devops_project/services/Iservices/IInvoiceDetailService.java
@@ -1,9 +1,6 @@
 package tn.esprit.devops_project.services.Iservices;
 
-import tn.esprit.devops_project.entities.Invoice;
 import tn.esprit.devops_project.entities.InvoiceDetail;
-import tn.esprit.devops_project.entities.Product;
-
 import java.util.List;
 
 public interface IInvoiceDetailService {
@@ -11,6 +8,6 @@ public interface IInvoiceDetailService {
     InvoiceDetail getInvoiceDetail(long idInvoiceDetail);
     InvoiceDetail addInvoiceDetail(InvoiceDetail invoiceDetail);
     void deleteInvoiceDetail(long idInvoiceDetail);
-    //Invoice addInvoiceDetailToInvoice(Long invoiceId, InvoiceDetail invoiceDetail);
+
 
 }
