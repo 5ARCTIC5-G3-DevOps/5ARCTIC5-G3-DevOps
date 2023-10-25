543ad7a8613b74de2b3d8952a2b50c5522fbff7a
sahar-letaief <sahar.letaief@esprit.tn>
2023-10-25 09:32:45 +0100

    removing comments

--- src/test/java/test/java/tn/esprit/devops_project/services/InvoiceDetailServiceImplTest.java
+++ src/test/java/test/java/tn/esprit/devops_project/services/InvoiceDetailServiceImplTest.java
@@ -36,65 +36,38 @@ import static org.mockito.Mockito.*;
         };
 
         when(invoiceDetailService.retriveAllInvoicesDetail()).thenReturn(invoiceDetailList);
-
-        // Test
         List<InvoiceDetail> invoiceDetailListTest = invoiceDetailService.retriveAllInvoicesDetail();
         assertEquals(3, invoiceDetailListTest.size());
         System.out.println("Retrieve all invoices details is working correctly...!!");
     }
     @Test
     void testGetInvoiceDetail() {
-        // Arrange
         InvoiceDetail invoiceDetail = new InvoiceDetail(4L, 15, 16F,null,null);
         when(invoiceDetailRepository.findById(invoiceDetail.getIdInvoiceDetail())).thenReturn(Optional.of(invoiceDetail));
-
-        // Act
         InvoiceDetail retrievedInvoiceDetail = invoiceDetailService.getInvoiceDetail(invoiceDetail.getIdInvoiceDetail());
-
-        // Assert
         assertNotNull(retrievedInvoiceDetail);
         assertSame(invoiceDetail, retrievedInvoiceDetail);
-
-        // Verify that invoiceRepository method was called
         verify(invoiceDetailRepository, times(1)).findById(invoiceDetail.getIdInvoiceDetail());
-
-       // when(invoiceDetailRepository.findById(invoiceDetail.getIdInvoiceDetail())).thenReturn(Optional.empty());
 
     }
 
 
     @Test
     void testAddInvoiceDetail() {
-        // Arrange
         InvoiceDetail invoiceDetail = new InvoiceDetail(5L, 5, 10.0F,null,null); // Create a sample InvoiceDetail
-
-        // Mock the behavior of the invoiceDetailRepository to return the saved InvoiceDetail
         when(invoiceDetailRepository.save(invoiceDetail)).thenReturn(invoiceDetail);
-
-        // Act
         InvoiceDetail savedInvoiceDetail = invoiceDetailService.addInvoiceDetail(invoiceDetail);
-
-        // Assert
-        assertNotNull(savedInvoiceDetail); // Verify that the saved InvoiceDetail is not null
-        assertSame(invoiceDetail, savedInvoiceDetail); // Verify that the saved InvoiceDetail is the same as the sample
-
-        // Verify that invoiceDetailRepository method was called
+        assertNotNull(savedInvoiceDetail);
+        assertSame(invoiceDetail, savedInvoiceDetail);
         verify(invoiceDetailRepository, times(1)).save(invoiceDetail);
         System.out.println("invoice detail added successfully ;");
     }
 
     @Test
     void testDeleteInvoiceDetail() {
-        // Arrange
         long idInvoiceDetailToDelete = 1L;
-
-        // Act
         invoiceDetailService.deleteInvoiceDetail(idInvoiceDetailToDelete);
-
-        // Verify that invoiceDetailRepository method was called to delete the InvoiceDetail
         verify(invoiceDetailRepository, times(1)).deleteById(idInvoiceDetailToDelete);
-
-        // Assert that the deletion was successful
         verifyNoMoreInteractions(invoiceDetailRepository);
         System.out.println("detail invoice is deleted successfully !!");
     }
--- src/test/java/test/java/tn/esprit/devops_project/services/InvoiceServiceImplTest.java
+++ src/test/java/test/java/tn/esprit/devops_project/services/InvoiceServiceImplTest.java
@@ -40,9 +40,6 @@ import java.util.List;
     @InjectMocks
     private InvoiceServiceImpl invoiceService;
 
-
-
-
     SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
     Set<InvoiceDetail> invoiceDetailSet = new HashSet<>() {
         {
@@ -55,8 +52,6 @@ import java.util.List;
 
     @Test
     void testRetrieveAllInvoices() throws ParseException {
-
-
         List<Invoice> invoiceList = new ArrayList<>() {
             {
                 add(new Invoice(2L, 12F, 15F, dateFormat.parse("2023-08-08"), dateFormat.parse("2023-09-09"), true,invoiceDetailSet,null));
@@ -66,8 +61,6 @@ import java.util.List;
         };
 
         when(invoiceService.retrieveAllInvoices()).thenReturn(invoiceList);
-
-        // Test
         List<Invoice> invoiceListTest = invoiceService.retrieveAllInvoices();
         assertEquals(3, invoiceListTest.size());
         System.out.println("Retrieve all invoices is working correctly...!!");
@@ -76,137 +69,72 @@ import java.util.List;
 
     @Test
     void testCancelInvoice() throws ParseException {
-        // Arrange
         Invoice invoice1=new Invoice(2L, 12F, 15F, dateFormat.parse("2023-08-08"), dateFormat.parse("2023-09-09"), true,invoiceDetailSet,null);
-
-
-        // Mock the behavior of the invoiceRepository
         when(invoiceRepository.findById(invoice1.getIdInvoice())).thenReturn(Optional.of(invoice1));
-
-        // Act
         invoiceService.cancelInvoice(invoice1.getIdInvoice());
-
-        // Assert
-        assertTrue(invoice1.getArchived()); // Verify that the invoice is archived
-
-        // Verify that invoiceRepository methods were called
+        assertTrue(invoice1.getArchived());
         verify(invoiceRepository, times(1)).findById(invoice1.getIdInvoice());
         verify(invoiceRepository, times(1)).save(invoice1);
-
-        // You can also use additional assertions to further validate the behavior
-        // For example, you can check if the invoiceRepository.updateInvoice method was not called, assuming it's not needed in this test.
-        //verify(invoiceRepository, never()).updateInvoice(any(Long.class));
 
     }
 
 
     @Test
     void testRetrieveInvoice() throws ParseException {
-        // Arrange
-        //Long idInvoice=6L;
         Invoice invoice2 = new Invoice(6L, 12F, 15F, dateFormat.parse("2023-08-08"), dateFormat.parse("2023-09-09"), true,invoiceDetailSet,null);
-
-        // Stub the behavior of the invoiceRepository to return invoice2 when findById is called with invoice2.getIdInvoice()
         when(invoiceRepository.findById(invoice2.getIdInvoice())).thenReturn(Optional.of(invoice2));
-
-        // Act
         invoiceService.retrieveInvoice(invoice2.getIdInvoice());
-
-        // Assert
         assertNotNull(invoice2);
-
-        // Verify that invoiceRepository method was called
         verify(invoiceRepository, times(1)).findById(invoice2.getIdInvoice());
 
 
     }
 
-
     @Test
     void testGetInvoicesBySupplier() throws ParseException {
-        // Arrange
         Long idSupplier = 1L;
         Supplier supplier = new Supplier(1L,"code","label",null,null); // Create a sample supplier
         Invoice invoice3 = new Invoice(7L, 12F, 15F, dateFormat.parse("2023-08-08"), dateFormat.parse("2023-09-09"), true, invoiceDetailSet, supplier);
-       Set<Invoice> invoiceSet=new HashSet<>();
-       invoiceSet.add(invoice3);
+        Set<Invoice> invoiceSet=new HashSet<>();
+        invoiceSet.add(invoice3);
         supplier.setInvoices(invoiceSet);
 
-        // Mock the behavior of the supplierRepository
         when(supplierRepository.findById(idSupplier)).thenReturn(Optional.of(supplier));
-//        when(invoiceRepository.findById(invoice3.getIdInvoice())).thenReturn(Optional.of(invoice3));
 
-        // Act
         List<Invoice> retrievedInvoices = invoiceService.getInvoicesBySupplier(idSupplier);
 
-        // Assert
-        assertNotNull(retrievedInvoices); // Verify that the returned list is not null
-        assertEquals(1, retrievedInvoices.size()); // Verify that one invoice is returned
-
-        // Verify that supplierRepository methods were called
+        assertNotNull(retrievedInvoices);
+        assertEquals(1, retrievedInvoices.size());
         verify(supplierRepository, times(1)).findById(idSupplier);
     }
-
-
-
-
-
-
-
-
-
-
     @Test
     void testAssignOperatorToInvoice()  throws ParseException {
-        // Arrange
         Long idOperator = 1L;
-       // Long idInvoice = 8L;
 
         Invoice invoice4=new Invoice(8L,12F,15F,dateFormat.parse("2023-08-08"),dateFormat.parse("2023-09-09"),true,invoiceDetailSet,null);
         Operator operator=new Operator(1L,"sahar","letaief","password",null);
-
-        // Mock the behavior of the repositories
         when(invoiceRepository.findById(invoice4.getIdInvoice())).thenReturn(Optional.of(invoice4));
         when(operatorRepository.findById(idOperator)).thenReturn(Optional.of(operator));
         Set<Invoice> invoiceSet=new HashSet<>();
         invoiceSet.add(invoice4);
         operator.setInvoices(invoiceSet);
         when(operatorRepository.save(operator)).thenReturn(operator);
-
-        // Act
         invoiceService.assignOperatorToInvoice(operator.getIdOperateur(),invoice4.getIdInvoice() );
-
-        // Assert
-        assertTrue(operator.getInvoices().contains(invoice4)); // Verify that the invoice is added to the operator's invoices
-
-        // Verify that repository methods were called
+        assertTrue(operator.getInvoices().contains(invoice4));
         verify(invoiceRepository, times(1)).findById(invoice4.getIdInvoice());
-//        verify(operatorRepository, times(1)).findById(idOperator);
-       // verify(operatorRepository, times(1)).save(operator);
-
-        // You can also use additional assertions to further validate the behavior
     }
 
 
 
     @Test
     void testGetTotalAmountInvoiceBetweenDates() throws ParseException{
-        // Arrange
 
         Date startDate = dateFormat.parse("2023-08-01");
         Date endDate = dateFormat.parse("2023-08-31");
         float expectedTotalAmount = 100.0F; // Set your expected total amount
-
-        // Mock the behavior of the invoiceRepository to return the expected total amount
         when(invoiceRepository.getTotalAmountInvoiceBetweenDates(startDate, endDate)).thenReturn(expectedTotalAmount);
-
-        // Act
         float totalAmount = invoiceService.getTotalAmountInvoiceBetweenDates(startDate, endDate);
-
-        // Assert
         assertEquals(expectedTotalAmount, totalAmount, 0.01); // Use a delta for float comparison
-
-        // Verify that invoiceRepository method was called
         verify(invoiceRepository, times(1)).getTotalAmountInvoiceBetweenDates(startDate, endDate);
     }
 
